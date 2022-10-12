2a031cec8f60562f4e3938a07ba82cd3a4433497
kevinelong <kevinelong@gmail.com>
2022-10-12 10:30:59 -0700

    format

--- loops/loop2_9.js
+++ loops/loop2_9.js
@@ -7,14 +7,14 @@
  
  FOR - PART1; PART2; PART3
 */
-for(let i = 0; i < 7; i++){
-    console.log(i, "I LOVE YOU");
-  }
-  
-  //WHILE WORKS SIMILARLY BUT THE PARTS ARE ON SEPARATE LINES
-  let i = 0; //WHERE TO START
-  while (i < 7) { //WHERE TO STOP - BOOLEAN EXPRESSION e.g is i less than seven?
-    console.log(i, "I LOVE YOU");
-    i++; //INCREMENT (go up by one like i=i+1)
-  }
+for (let i = 0; i < 7; i++) {
+  console.log(i, "I LOVE YOU");
+}
+
+//WHILE WORKS SIMILARLY BUT THE PARTS ARE ON SEPARATE LINES
+let i = 0; //WHERE TO START
+while (i < 7) { //WHERE TO STOP - BOOLEAN EXPRESSION e.g is i less than seven?
+  console.log(i, "I LOVE YOU");
+  i++; //INCREMENT (go up by one like i=i+1)
+}
   //STOP
